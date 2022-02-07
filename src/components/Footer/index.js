2ae44b1c05fe0ef4e680de8fd9843d116673baac
Abhishek Bharadwaz <abhishekbharadwaz22@gmail.com>
2022-02-08 02:54:11 +0530

    final commit

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,10 +5,9 @@ const Footer = () => {
     return (
         <div className={styles['footer']}>
             <span className={styles['links']}>
-                <a href="https://github.com/4Marsha1/WikiSearch">Source Code</a> |
-                <a href="https://wiki-search-api.netlify.app/">Deployed SIte</a> |
-                <a href="https://github.com/4Marsha1/"><i class="fab fa-github"></i>  Github</a> |
-                <a href="https://www.linkedin.com/in/abhishek-bharadwaz-458993192/"><i class="fab fa-linkedin"></i>  LinkedIn</a>
+                <a href="https://github.com/4Marsha1/WikiSearch" target="_blank">Source Code</a> |
+                <a href="https://github.com/4Marsha1/" target="_blank"><i class="fab fa-github"></i>  Github</a> |
+                <a href="https://www.linkedin.com/in/abhishek-bharadwaz-458993192/" target="_blank"><i class="fab fa-linkedin"></i>  LinkedIn</a>
             </span>
             <span className={styles['copyright']}>&copy; Abhishek Bharadwaz 2022</span>
         </div>
