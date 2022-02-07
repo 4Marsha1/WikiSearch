31792149e6a2157ad8a647a62c98ced807e32051
Abhishek Bharadwaz <abhishekbharadwaz22@gmail.com>
2022-02-08 02:48:32 +0530

    deployed

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 # Project using API
 
-### Technology used: ReactJs & ReduxJS
+---
+
+### Technology used:
+
+`ReactJs` & `ReduxJS`
 
 ### API Used :
 
 ```
 https://en.wikipedia.org/w/api.php?action=query&generator=search
 ```
+
+[Deployed Site](https://wiki-search-api.netlify.app/)
+
+> Happy Coding!
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,10 +5,10 @@ const Footer = () => {
     return (
         <div className={styles['footer']}>
             <span className={styles['links']}>
-                <a href="#">Source Code</a> |
-                <a href="#">Deployed SIte</a> |
-                <a href="#"><i class="fab fa-github"></i>  Github</a> |
-                <a href="#"><i class="fab fa-linkedin"></i>  LinkedIn</a>
+                <a href="https://github.com/4Marsha1/WikiSearch">Source Code</a> |
+                <a href="https://wiki-search-api.netlify.app/">Deployed SIte</a> |
+                <a href="https://github.com/4Marsha1/"><i class="fab fa-github"></i>  Github</a> |
+                <a href="https://www.linkedin.com/in/abhishek-bharadwaz-458993192/"><i class="fab fa-linkedin"></i>  LinkedIn</a>
             </span>
             <span className={styles['copyright']}>&copy; Abhishek Bharadwaz 2022</span>
         </div>
